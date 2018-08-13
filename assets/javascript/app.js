8cbb4d5d7058d7d243e36f06c7e2998d8a3983e7
AntonSemenov <AntonSemenov@Antons-MacBook-Pro.local>
2018-08-12 22:56:52 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,12 +7,21 @@ $(document).ready(function () {
         ('clicked', $('#header').hide());
         ('clicked', $('#header2').text("Answer all questions until the time runs out"));
 
-    });
-
-    //adding content when start button was clicked:
-    $('#start-button').on('click', function (event) {
-
 
     });
+    var counter = 0;
+    function setup() {
+        noCanvas();
+        var timer = select("#timer");
+        timer.html(counter);
+
+        function timeIt() {
+            counter++;
+            timer.html(counter);
+
+        }
+
+        setInterval(timeIt, 1000);
+    }
 
 })
