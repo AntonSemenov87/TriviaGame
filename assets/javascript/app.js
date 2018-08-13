663872b5b4af6e57bd2eb82aaa1eaa7daff6289d
AntonSemenov <AntonSemenov@Antons-MacBook-Pro.local>
2018-08-12 23:37:26 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     //removing containers when Start button is clicked
     $('#start-button').on('click', function (event) {
         ('clicked', $('#start-button').hide());
@@ -7,21 +8,43 @@ $(document).ready(function () {
         ('clicked', $('#header').hide());
         ('clicked', $('#header2').text("Answer all questions until the time runs out"));
 
+        // questions:
+        var options = [
+            {
+                question: "Apple was founded in:",
+                choice: ["1976", "1986", "1996", "2006"],
+                answer: 0,
+            },
+            {
+                question: "What year first iPhone was released?",
+                choice: ["1997", "2007", "2006", "2009"],
+                answer: 1,
+            },
+            {
+                question: "Who was one of the founders of Apple?",
+                choice: ["J.Travolta", "R.Martin", "S.Jobs", "T.Hanks"],
+                answer: 2,
+            },
+            {
+                question: "What was the name of small iPod?",
+                choice: ["iPod Pro", "iPod mini", "iPod shuffle", "iPod Grande"],
+                answer: 2,
+                photo: "assets/images/harvey.jpg"
+            },
+            {
+                question: "What is the name of watch made by Apple?",
+                choice: ["iWatch", "uWatch", "AWatch", "AppleWatch"],
+                answer: 3,
+            },
+            {
+                question: "When was the first iPad released?",
+                choice: ["2009", "2010", "2011", "2012"],
+                answer: 1,
+
+            }];
+
 
     });
-    var counter = 0;
-    function setup() {
-        noCanvas();
-        var timer = select("#timer");
-        timer.html(counter);
 
-        function timeIt() {
-            counter++;
-            timer.html(counter);
-
-        }
-
-        setInterval(timeIt, 1000);
-    }
 
 })
